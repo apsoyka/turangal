2ad8b584642aca258f37e216f0cf6dbb4c23a7b9
Daniel Douglas Soyka <ddsoyka@pm.me>
2021-06-01 20:35:25 -0400

    Parse and Display Unique Hosts

--- run.sh
+++ run.sh
@@ -1,20 +1,58 @@
 #!/bin/bash
 
-if [ -z "$@" ]
+function print_hosts() {
+    pattern="^(?:http|https):\/\/(?:[a-z0-9]+\.)*\K([a-z0-9]+\.[a-z0-9]+)"
+    hosts=()
+
+    # Use a regular expression to extract the domain name from each URI
+    for host in "$@"
+    do
+        match=$(echo $host | grep -oP $pattern)
+        hosts+=($match)
+    done
+
+    # Remove duplicates
+    hosts=($(printf "%s\n" "${hosts[@]}" | sort -u))
+
+    # Get size of array
+    count=${#hosts[@]}
+
+    if [ "$count" -gt "0" ]
+    then
+        echo "Mirroring content from ${count} hosts:"
+
+        # Print each host
+        for host in "${hosts[@]}"
+        do
+            echo "$host"
+        done
+    else
+        echo "Could not find any hosts"
+    fi
+}
+
+if [ "$#" -eq "0" ]
 then
-    echo "No URL resources were provided; exiting"
+    echo "No URI resources were provided; exiting"
 else
+    print_hosts "$@"
+
     echo "Starting Tor"
 
     service tor start
 
-    echo "Mirroring content hosted at $@"
+    date=$(date +"%Y-%m-%d")
+    filename="$date"
 
-    torsocks wget \
-    -e robots=off \
+    torsocks \
+    wget \
+    --execute robots=off \
+    --warc-file="$filename" \
+    --no-warc-keep-log \
     --timeout=5 \
     --tries=10 \
     --adjust-extension \
+    --html-extension \
     --content-disposition \
     --convert-links \
     --mirror \
